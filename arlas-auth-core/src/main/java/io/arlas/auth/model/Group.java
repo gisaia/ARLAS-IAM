cef5f1a6c7e4a30f6fc1ed547a208adb7afb8ba8
alainbodiguel <alain.bodiguel@gisaia.com>
2021-12-20 18:02:01 +0100

    More models (groups)

--- arlas-auth-core/src/main/java/io/arlas/auth/model/Group.java
+++ arlas-auth-core/src/main/java/io/arlas/auth/model/Group.java
@@ -1,4 +1,61 @@
 package io.arlas.auth.model;
 
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "group")
 public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @EmbeddedId
+    private GroupKey key;
+
+    @ManyToMany()
+    @JoinTable(name = "GroupMember",
+            joinColumns = @JoinColumn(name = "id_group"),
+            inverseJoinColumns = @JoinColumn(name = "id_user"))
+    private Set<User> members = new HashSet<>();
+
+    private Group() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public GroupKey getKey() {
+        return key;
+    }
+
+    public void setKey(GroupKey key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return key.getName();
+    }
+
+    public void setName(String name) {
+        getKey().setName(name);
+    }
+
+    public Organisation getOrganisation() {
+        return getKey().getOrganisation();
+    }
+
+    public void setOrganisation(Organisation organisation) {
+        getKey().setOrganisation(organisation);
+    }
+
+    public Set<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(Set<User> members) {
+        this.members = members;
+    }
 }
