de548115950ac8b7c05514ec0bd674a2755259b9
alainbodiguel <alain.bodiguel@gisaia.com>
2021-12-21 09:40:35 +0100

    More models (roles)

--- arlas-auth-core/src/main/java/io/arlas/auth/model/Role.java
+++ arlas-auth-core/src/main/java/io/arlas/auth/model/Role.java
@@ -1,4 +1,89 @@
 package io.arlas.auth.model;
 
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "role")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @NotNull
+    @Column(unique = true)
+    private String name;
+
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    @Column(name="is_system")
+    private boolean isSystem = false; // system roles are shared among all organisations
+
+    @ManyToMany()
+    @JoinTable(name = "OrganisationRole",
+            joinColumns = @JoinColumn(name = "id_role"),
+            inverseJoinColumns = @JoinColumn(name = "id_organisation"))
+    private Set<Organisation> organisations = new HashSet<>();
+
+    @ManyToMany()
+    @JoinTable(name = "GroupRole",
+            joinColumns = @JoinColumn(name = "id_role"),
+            inverseJoinColumns = @JoinColumn(name = "id_group"))
+    private Set<Group> groups = new HashSet<>();
+
+    @ManyToMany()
+    @JoinTable(name = "UserRole",
+            joinColumns = @JoinColumn(name = "id_role"),
+            inverseJoinColumns = @JoinColumn(name = "id_user"))
+    private Set<User> users = new HashSet<>();
+
+    private Role() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSystem() {
+        return isSystem;
+    }
+
+    public void setSystem(boolean isSystem) {
+        this.isSystem = isSystem;
+    }
+
+    public Set<Organisation> getOrganisations() {
+        return organisations;
+    }
+
+    public void setOrganisations(Set<Organisation> organisations) {
+        this.organisations = organisations;
+    }
+
+    public Set<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(Set<Group> groups) {
+        this.groups = groups;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
 }
