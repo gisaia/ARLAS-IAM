174c794ee6a64eed43ff992d8222586f4371bee9
alainbodiguel <alain.bodiguel@gisaia.com>
2021-12-23 18:25:02 +0100

    Auth service: cleanup

--- arlas-auth-core/src/main/java/io/arlas/auth/impl/HibernateAuthService.java
+++ arlas-auth-core/src/main/java/io/arlas/auth/impl/HibernateAuthService.java
@@ -50,10 +50,29 @@ public class HibernateAuthService implements AuthService {
         return regexMatcher.find() ? Optional.of(regexMatcher.group()) : Optional.empty();
     }
 
+    private void sendActivationEmail(User user) {
+        // TODO
+    }
+
+    private Organisation getOrganisation(User owner, UUID orgId, boolean checkOwned)
+            throws NotOwnerException, NotFoundException {
+        Optional<Organisation> organisation = owner.getOrganisations().stream()
+                .filter(om -> (om.getOrganisation().is(orgId)) && (!checkOwned || om.isOwner()))
+                .map(OrganisationMember::getOrganisation)
+                .findFirst();
+        if (organisation.isPresent()) {
+            return organisation.get();
+        } else {
+            if (checkOwned) { throw new NotOwnerException(); }
+            else { throw new NotFoundException(); }
+        }
+    }
+
     // ------- public ------------
 
     @Override
-    public User createUser(String email) throws InvalidEmailException, AlreadyExistsException {
+    public User createUser(String email)
+            throws InvalidEmailException, AlreadyExistsException {
         if (validateEmailDomain(email).isPresent()) {
             if (userDao.readUser(email).isEmpty()) {
                 User user = new User();
@@ -69,20 +88,17 @@ public class HibernateAuthService implements AuthService {
         }
     }
 
-    private void sendActivationEmail(User user) {
-        // TODO
-    }
-
     @Override
     public Optional<User> readUser(UUID userId) {
         return userDao.readUser(userId);
     }
 
     @Override
-    public User login(String email, String password) throws NotFoundException {
-        Optional<User> user = userDao.readUser(email);
-        if (user.isPresent() && user.get().isActive() && matches(password, user.get().getPassword())) {
-            return user.get();
+    public User login(String email, String password)
+            throws NotFoundException {
+        User user = userDao.readUser(email).orElseThrow(NotFoundException::new);
+        if (user.isActive() && matches(password, user.getPassword())) {
+            return user;
         } else {
             // we don't tell the user which of email or password is wrong, to avoid "username enumeration" attack type
             throw new NotFoundException();
@@ -90,7 +106,8 @@ public class HibernateAuthService implements AuthService {
     }
 
     @Override
-    public User updateUser(User user, String oldPassword, String newPassword) throws NonMatchingPasswordException {
+    public User updateUser(User user, String oldPassword, String newPassword)
+            throws NonMatchingPasswordException {
         if (matches(oldPassword, user.getPassword())) {
             user.setPassword(encode(newPassword));
             return userDao.updateUser(user);
@@ -118,10 +135,12 @@ public class HibernateAuthService implements AuthService {
 
     @Override
     public Optional<User> verifyUser(UUID userId, String password) {
+        // TODO add a token system to validate the verification request (link from verification email)
         Optional<User> user = readUser(userId);
         user.ifPresent(u -> {
             u.setPassword(encode(password));
             u.setVerified(true);
+            // TODO create personal organisation: what name? avoid email for GPRD
             userDao.updateUser(u);
         });
         return user;
@@ -143,7 +162,8 @@ public class HibernateAuthService implements AuthService {
     }
 
     @Override
-    public Organisation createOrganisation(User owner) throws AlreadyExistsException, NotOwnerException {
+    public Organisation createOrganisation(User owner)
+            throws AlreadyExistsException, NotOwnerException {
         String domain = validateEmailDomain(owner.getEmail()).orElseThrow(RuntimeException::new);
         Optional<Organisation> org = organisationDao.readOrganisation(domain);
         if (org.isEmpty()) {
@@ -161,7 +181,8 @@ public class HibernateAuthService implements AuthService {
     }
 
     @Override
-    public Organisation deleteOrganisation(User owner, UUID orgId) throws NotOwnerException, NotFoundException {
+    public Organisation deleteOrganisation(User owner, UUID orgId)
+            throws NotOwnerException, NotFoundException {
         Organisation organisation = getOrganisation(owner, orgId, true);
         organisationDao.deleteOrganisation(organisation);
         // TODO : delete associated resources
@@ -173,21 +194,9 @@ public class HibernateAuthService implements AuthService {
         return userDao.listOrganisations(user);
     }
 
-    private Organisation getOrganisation(User owner, UUID orgId, boolean checkOwned) throws NotOwnerException, NotFoundException {
-        Optional<Organisation> organisation = owner.getOrganisations().stream()
-                .filter(om -> (om.getOrganisation().is(orgId)) && (!checkOwned || om.isOwner()))
-                .map(OrganisationMember::getOrganisation)
-                .findFirst();
-        if (organisation.isPresent()) {
-            return organisation.get();
-        } else {
-            if (checkOwned) { throw new NotOwnerException(); }
-            else { throw new NotFoundException(); }
-        }
-    }
-
     @Override
-    public Organisation addUserToOrganisation(User owner, String email, UUID orgId) throws NotOwnerException, NotFoundException {
+    public Organisation addUserToOrganisation(User owner, String email, UUID orgId)
+            throws NotOwnerException, NotFoundException {
         return organisationMemberDao.addUserToOrganisation(
                 userDao.readUser(email).orElseThrow(NotFoundException::new),
                 getOrganisation(owner, orgId, true),
@@ -195,14 +204,16 @@ public class HibernateAuthService implements AuthService {
     }
 
     @Override
-    public Organisation removeUserFromOrganisation(User owner, UUID userId, UUID orgId) throws NotOwnerException, NotFoundException {
+    public Organisation removeUserFromOrganisation(User owner, UUID userId, UUID orgId)
+            throws NotOwnerException, NotFoundException {
         return organisationMemberDao.removeUserFromOrganisation(
                 userDao.readUser(userId).orElseThrow(NotFoundException::new),
                 getOrganisation(owner, orgId, true));
     }
 
     @Override
-    public Role createRole(User owner, String name, UUID orgId, Set<Permission> permissions) throws AlreadyExistsException, NotOwnerException, NotFoundException {
+    public Role createRole(User owner, String name, UUID orgId, Set<Permission> permissions)
+            throws AlreadyExistsException, NotOwnerException, NotFoundException {
         Organisation organisation = getOrganisation(owner, orgId, true);
         if (organisation.getRoles().stream().anyMatch(r -> r.getName().equals(name))) {
             throw new AlreadyExistsException();
@@ -212,7 +223,8 @@ public class HibernateAuthService implements AuthService {
     }
 
     @Override
-    public User addRoleToUser(User owner, UUID orgId, UUID userId, UUID roleId) throws NotFoundException, NotOwnerException {
+    public User addRoleToUser(User owner, UUID orgId, UUID userId, UUID roleId)
+            throws NotFoundException, NotOwnerException {
         Organisation ownerOrg = getOrganisation(owner, orgId, true);
         User user = userDao.readUser(userId).orElseThrow(NotFoundException::new);
         getOrganisation(user, orgId, false);
@@ -222,7 +234,8 @@ public class HibernateAuthService implements AuthService {
     }
 
     @Override
-    public User removeRoleFromUser(User owner, UUID orgId, UUID userId, UUID roleId) throws NotOwnerException, NotFoundException {
+    public User removeRoleFromUser(User owner, UUID orgId, UUID userId, UUID roleId)
+            throws NotOwnerException, NotFoundException {
         Organisation ownerOrg = getOrganisation(owner, orgId, true);
         User user = userDao.readUser(userId).orElseThrow(NotFoundException::new);
         getOrganisation(user, orgId, false);
@@ -232,7 +245,8 @@ public class HibernateAuthService implements AuthService {
     }
 
     @Override
-    public Group createGroup(User owner, String name, UUID orgId) throws AlreadyExistsException, NotOwnerException, NotFoundException {
+    public Group createGroup(User owner, String name, UUID orgId)
+            throws AlreadyExistsException, NotOwnerException, NotFoundException {
         Organisation organisation = getOrganisation(owner, orgId, true);
         if (organisation.getGroups().stream().anyMatch(r -> r.getName().equals(name))) {
             throw new AlreadyExistsException();
@@ -263,18 +277,18 @@ public class HibernateAuthService implements AuthService {
 
     @Override
     public Group addRoleToGroup(User owner, UUID orgId, UUID roleId, UUID grpId) throws NotOwnerException, NotFoundException {
-        Organisation ownerOrg = getOrganisation(owner, orgId, true);
-        Role role = ownerOrg.getRoles().stream().filter(r -> r.is(roleId)).findFirst().orElseThrow(NotFoundException::new);
-        Group group = ownerOrg.getGroups().stream().filter(g -> g.is(grpId)).findFirst().orElseThrow(NotFoundException::new);
-        return groupDao.addRoleToGroup(role, group);
+        Organisation org = getOrganisation(owner, orgId, true);
+        return groupDao.addRoleToGroup(
+                org.getRoles().stream().filter(r -> r.is(roleId)).findFirst().orElseThrow(NotFoundException::new),
+                org.getGroups().stream().filter(g -> g.is(grpId)).findFirst().orElseThrow(NotFoundException::new));
     }
 
     @Override
     public Group removeRoleFromGroup(User owner, UUID orgId, UUID roleId, UUID grpId) throws NotOwnerException, NotFoundException {
-        Organisation ownerOrg = getOrganisation(owner, orgId, true);
-        Role role = ownerOrg.getRoles().stream().filter(r -> r.is(roleId)).findFirst().orElseThrow(NotFoundException::new);
-        Group group = ownerOrg.getGroups().stream().filter(g -> g.is(grpId)).findFirst().orElseThrow(NotFoundException::new);
-        return groupDao.removeRoleFromGroup(role, group);
+        Organisation org = getOrganisation(owner, orgId, true);
+        return groupDao.removeRoleFromGroup(
+                org.getRoles().stream().filter(r -> r.is(roleId)).findFirst().orElseThrow(NotFoundException::new),
+                org.getGroups().stream().filter(g -> g.is(grpId)).findFirst().orElseThrow(NotFoundException::new));
     }
 
     @Override
@@ -286,24 +300,28 @@ public class HibernateAuthService implements AuthService {
         user.getRoles().stream()
                 .filter(r -> r.getOrganisations().contains(ownerOrg))
                 .forEach(r -> permissions.addAll(r.getPermissions()));
+        user.getGroups().stream()
+                .filter(g -> g.getOrganisation().is(orgId))
+                .forEach(g -> g.getRoles().forEach(r -> permissions.addAll(r.getPermissions())));
         return permissions;
     }
 
     @Override
     public Permission createPermission(String permission, boolean isSystem) {
-        permissionDao.createPermission(new Permission(permission, isSystem));
-        return null;
+        return permissionDao.createPermission(new Permission(permission, isSystem));
     }
 
     @Override
-    public User addPermissionToUser(UUID userId, UUID permissionId) throws NotFoundException {
+    public User addPermissionToUser(UUID userId, UUID permissionId)
+            throws NotFoundException {
         User user = userDao.readUser(userId).orElseThrow(NotFoundException::new);
         Permission permission = permissionDao.readPermission(permissionId).orElseThrow(NotFoundException::new);
         return userDao.addPermissionToUser(user, permission);
     }
 
     @Override
-    public User removePermissionFromUser(UUID userId, UUID permissionId) throws NotFoundException {
+    public User removePermissionFromUser(UUID userId, UUID permissionId)
+            throws NotFoundException {
         User user = userDao.readUser(userId).orElseThrow(NotFoundException::new);
         Permission permission = permissionDao.readPermission(permissionId).orElseThrow(NotFoundException::new);
         return userDao.removePermissionFromUser(user, permission);
