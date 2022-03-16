300aef45656197421f37f8f883367e8839b6d7aa
alainbodiguel <alain.bodiguel@gisaia.com>
2022-03-16 16:38:03 +0100

    patch CI scripts: fix docker server

--- scripts/ci/publish_docker.sh
+++ scripts/ci/publish_docker.sh
@@ -15,13 +15,13 @@ ${SCRIPT_DIRECTORY}/build_maven.sh ${RELEASE_VERSION}
 echo "=> Build arlas-idp-server:${RELEASE_VERSION} docker image"
 docker build -t docker.cloudsmith.io/gisaia/private/arlas-idp-server:${RELEASE_VERSION} -f ${PROJECT_ROOT_DIRECTORY}/docker/docker-files/Dockerfile-idp .
 echo "=> Build arlas-server:${RELEASE_VERSION} docker image"
-docker build --build-arg TAG=${ARLAS_VERSION} -t docker.cloudsmith.io/gisaia/arlas-enterprise/arlas-server:${RELEASE_VERSION} -f ${PROJECT_ROOT_DIRECTORY}/docker/docker-files/Dockerfile-arlas-server .
+docker build --build-arg TAG=${ARLAS_VERSION} -t docker.cloudsmith.io/gisaia/private/arlas-server:${RELEASE_VERSION} -f ${PROJECT_ROOT_DIRECTORY}/docker/docker-files/Dockerfile-arlas-server .
 echo "=> Docker login cloudsmith"
 echo "${DOCKER_CLOUDSMITH_PASSWORD}" | docker login docker.cloudsmith.io -u ${DOCKER_CLOUDSMITH_USERNAME} --password-stdin
 echo "=> Push arlas-idp-server:${RELEASE_VERSION} docker images"
 docker push docker.cloudsmith.io/gisaia/private/arlas-idp-server:${RELEASE_VERSION}
 echo "=> Push arlas-server:${RELEASE_VERSION} docker images"
-docker push docker.cloudsmith.io/gisaia/arlas-enterprise/arlas-server:${RELEASE_VERSION}
+docker push docker.cloudsmith.io/gisaia/private/arlas-server:${RELEASE_VERSION}
 
 IFS='-' # - is set as delimiter
 read -ra SEMVER_PARTS <<< "$RELEASE_VERSION" # $RELEASE_VERSION is read into an array as tokens separated by IFS
