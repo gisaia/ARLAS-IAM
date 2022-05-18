ee3631e851f26e0781d4c420740930a08721c2a5
alainbodiguel <alain.bodiguel@gisaia.com>
2022-05-18 20:17:10 +0200

    set version 22.0.0-beta.5

--- scripts/ci/publish_docker_local.sh
+++ scripts/ci/publish_docker_local.sh
@@ -7,9 +7,9 @@ SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 PROJECT_ROOT_DIRECTORY="$SCRIPT_DIRECTORY"/../..
 
 echo "=> Build arlas-server:${RELEASE_VERSION} docker image"
-docker build --build-arg TAG=22.0.0-beta.4 -t docker.cloudsmith.io/gisaia/private/arlas-server:${RELEASE_VERSION} -f ${PROJECT_ROOT_DIRECTORY}/docker/docker-files/Dockerfile-arlas-server .
+docker build --build-arg TAG=22.0.0-beta.5 -t docker.cloudsmith.io/gisaia/private/arlas-server:${RELEASE_VERSION} -f ${PROJECT_ROOT_DIRECTORY}/docker/docker-files/Dockerfile-arlas-server .
 echo "=> Build arlas-persistence:${RELEASE_VERSION} docker image"
-docker build --build-arg TAG=22.0.0-beta.4 -t docker.cloudsmith.io/gisaia/private/arlas-persistence-server:${RELEASE_VERSION} -f ${PROJECT_ROOT_DIRECTORY}/docker/docker-files/Dockerfile-arlas-persistence .
+docker build --build-arg TAG=22.0.0-beta.5 -t docker.cloudsmith.io/gisaia/private/arlas-persistence-server:${RELEASE_VERSION} -f ${PROJECT_ROOT_DIRECTORY}/docker/docker-files/Dockerfile-arlas-persistence .
 echo "=> Build arlas-permissions:${RELEASE_VERSION} docker image"
-docker build --build-arg TAG=22.0.0-beta.4 -t docker.cloudsmith.io/gisaia/private/arlas-permissions-server:${RELEASE_VERSION} -f ${PROJECT_ROOT_DIRECTORY}/docker/docker-files/Dockerfile-arlas-permissions .
+docker build --build-arg TAG=22.0.0-beta.5 -t docker.cloudsmith.io/gisaia/private/arlas-permissions-server:${RELEASE_VERSION} -f ${PROJECT_ROOT_DIRECTORY}/docker/docker-files/Dockerfile-arlas-permissions .
 
