26cc83d9b80288aafb1f859dfedb4cb4f7c5d3e8
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2024-09-06 11:23:23 +0200

    feat: remove cloudsmith dependency

--- scripts/ci/publish_docker.sh
+++ scripts/ci/publish_docker.sh
@@ -12,19 +12,16 @@ echo "=> ARLAS_VERSION:${ARLAS_VERSION}"
 ${SCRIPT_DIRECTORY}/build_maven.sh ${RELEASE_VERSION}
 
 echo "=> Build arlas-iam-server:${RELEASE_VERSION} docker image"
-docker build -t docker.cloudsmith.io/gisaia/private/arlas-iam-server:${RELEASE_VERSION} -f ${PROJECT_ROOT_DIRECTORY}/docker/docker-files/Dockerfile .
-
-echo "=> Docker login cloudsmith"
-echo "${DOCKER_CLOUDSMITH_PASSWORD}" | docker login docker.cloudsmith.io -u ${DOCKER_CLOUDSMITH_USERNAME} --password-stdin
+docker build -t gisaia/arlas-iam-server:${RELEASE_VERSION} -f ${PROJECT_ROOT_DIRECTORY}/docker/docker-files/Dockerfile .
 
 echo "=> Push arlas-iam-server:${RELEASE_VERSION} docker images"
-docker push docker.cloudsmith.io/gisaia/private/arlas-iam-server:${RELEASE_VERSION}
+docker push gisaia/arlas-iam-server:${RELEASE_VERSION}
 
 IFS='-' # - is set as delimiter
 read -ra SEMVER_PARTS <<< "$RELEASE_VERSION" # $RELEASE_VERSION is read into an array as tokens separated by IFS
 if [ "${#SEMVER_PARTS[@]}" -eq "1" ]; then
   # no pre-release found in semantic version => it's a release
   echo "=> Tag arlas-iam-server:latest docker image"
-  docker tag docker.cloudsmith.io/gisaia/private/arlas-iam-server:${RELEASE_VERSION} docker.cloudsmith.io/gisaia/private/arlas-iam-server:latest
-  docker push docker.cloudsmith.io/gisaia/private/arlas-iam-server:latest
+  docker tag gisaia/arlas-iam-server:${RELEASE_VERSION} gisaia/arlas-iam-server:latest
+  docker push gisaia/arlas-iam-server:latest
 fi
