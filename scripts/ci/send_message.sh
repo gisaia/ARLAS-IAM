e5dd1d5d440522511fe896b26cd33b0546f54c41
MohamedHamouGisaia <mohamed.hamou@gisaia.com>
2025-01-31 16:35:14 +0100

    Send message at the end of the release

---
+++ scripts/ci/send_message.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+RELEASE_VERSION=$1
+
+send_chat_message(){
+    MESSAGE=$1
+    if [ -z "$GOOGLE_CHAT_RELEASE_CHANEL" ] ; then
+        echo "Environement variable GOOGLE_CHAT_RELEASE_CHANEL is not definied ... skipping message publishing"
+    else
+        DATA='{"text":"'${MESSAGE}'"}'
+        echo $DATA
+        curl -X POST --header "Content-Type:application/json" $GOOGLE_CHAT_RELEASE_CHANEL -d "${DATA}"
+    fi
+}
+
+send_chat_message "Release of ARLAS-IAM, version ${RELEASE_VERSION}"
